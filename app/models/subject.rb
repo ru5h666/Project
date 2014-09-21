4593d7a3d1537fc57d082b99314085fe1aa02682
Alex Relph <alex.relph@sky.com>
2014-09-21 23:13:16 +0100

    initial commit

---
+++ app/models/subject.rb
@@ -0,0 +1,4 @@
+class Subject < ActiveRecord::Base
+  # attr_accessible :title, :body
+  has_many :posts
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project::Application.config.secret_token = '2e9c08520b5418e9b92a4fe8a7aaa0422d8c67969a4ab66189ab47151344f9d7d2c3a749327d43809692fa243457446b41ac47f29e0f65e74e9272ad3c3a0181'
